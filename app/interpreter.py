ce511e0912d8518049503cdda13617ce6eea0e64
thegostisdead <thegostisdead@gmail.com>
2024-12-27 15:22:37 +0100

    feat: runtime errors. fuck python again

--- app/interpreter.py
+++ app/interpreter.py
@@ -5,11 +5,7 @@ from typing import Any
 
 class Interpreter(Visitor):
     def _is_truthy(self, obj: Any) -> bool:
-        if obj is None :
-            return False
-        if type(obj) == type(True):
-            return bool(obj)
-        return True
+        return bool(obj)
 
     def _is_equal(self, a : Any, b : Any):
         if a is None and b is None:
@@ -18,15 +14,19 @@ class Interpreter(Visitor):
             return False
 
         return a == b
+
+    @staticmethod
+    def _is_number(obj: Any) -> bool:
+        return isinstance(obj, (int, float)) and not isinstance(obj, bool)
     def _evaluate(self, expr: Expr):
         return expr.accept(self)
 
     def _check_number_operand(self, operator: Token,  operand: Any):
-        if isinstance(operand, float) : return
+        if self._is_number(operand) : return
         raise LoxRuntimeError(operator, "Operand must be a number.")
 
     def _check_number_operands(self, operator: Token, left: Any, right: Any):
-        if  isinstance(left, float) and isinstance(right, float) : return
+        if  self._is_number(left) and self._is_number(right) : return
         raise LoxRuntimeError(operator, "Operands must be numbers.")
 
     def visit_literal_expr(self, expr: Expr):
@@ -45,38 +45,44 @@ class Interpreter(Visitor):
 
         if expr.operator.token_type == TokenType.GREATER:
             self._check_number_operands(expr.operator, left, right)
-            return float(left) > float(right)
+            return left > right
 
         if expr.operator.token_type == TokenType.GREATER_EQUAL:
             self._check_number_operands(expr.operator, left, right)
-            return float(left) >=  float(right)
+            return left >= right
 
         if expr.operator.token_type == TokenType.LESS:
             self._check_number_operands(expr.operator, left, right)
-            return float(left) < float(right)
+            return left < right
 
         if expr.operator.token_type == TokenType.LESS_EQUAL:
             self._check_number_operands(expr.operator, left, right)
-            return float(left) <= float(right)
+            return left <= right
 
         if expr.operator.token_type == TokenType.MINUS :
             self._check_number_operands(expr.operator, left, right)
-            return float(left) - float(right)
+            return left - right
 
         if expr.operator.token_type == TokenType.PLUS :
-            if isinstance(left, float) and isinstance(right, float) :
-                return float(left) + float(right)
+            if self._is_number(left) and self._is_number(right) :
+                return left + right
+
+            # for string concat
             if isinstance(left, str) and isinstance(right, str) :
-                return str(left) + str(right)
+                return left + right
+
             raise LoxRuntimeError(expr.operator,"Operands must be two numbers or two strings.")
 
         if expr.operator.token_type == TokenType.SLASH:
-            return float(left) / float(right)
+            if self._is_number(left) and self._is_number(right):
+                return left / right
+            raise LoxRuntimeError(expr.operator,"Operands must be numbers.")
 
-        if expr.operator.token_type ==TokenType.STAR :
-            self._check_number_operands(expr.operator, left, right)
-            return float(left) * float(right)
-        # Unreachable
+        if expr.operator.token_type == TokenType.STAR :
+            if self._check_number_operands(expr.operator, left, right):
+                return left * right
+            raise LoxRuntimeError(expr.operator, "Operands must be numbers.")
+
         return None
 
 
@@ -89,19 +95,20 @@ class Interpreter(Visitor):
             return not self._is_truthy(right)
         if expr.operator.token_type == TokenType.MINUS:
             self._check_number_operand(expr.operator, right)
-            return - float(right)
+            return - right
 
-        # Unreachable
         return None
 
     def _stringify(self, obj: Any):
+        if isinstance(obj, str):
+            return obj
+
         if obj is None :
-            return "nil"
-        if isinstance(obj, float):
-            text = str(obj)
-            if text.endswith(".0") :
-                text = text[0:len(text) - 2]
-            return text
+            return "null"
+
+        if isinstance(obj, bool):
+            return str(obj).lower()
+
         return str(obj)
 
     def interpret(self, expr: Expr):
